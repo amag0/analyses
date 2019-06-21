cca88e806432ce358ed9232489396bcda07d2196
amag0 <30939985+amag0@users.noreply.github.com>
2019-06-21 23:40:11 +0200

    define a function object - function.h

---
+++ function.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+class function
+{
+// first define the public interface
+public:
+	float operator()(float a, float b, float c);
+	/* so much empty */
+};
+
+#endif
